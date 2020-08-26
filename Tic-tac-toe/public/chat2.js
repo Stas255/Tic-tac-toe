ae45d74c4036af71278d7f78d7d1635bb1e91694
godlyfrenemy <61899504+1vitality1@users.noreply.github.com>
2020-08-26 20:58:17 +0300

    Draw

--- Tic-tac-toe/public/chat2.js
+++ Tic-tac-toe/public/chat2.js
@@ -73,7 +73,6 @@ $(function () {
             UpdateMap(data);
             Message("Socket Answer update_Map_", data.map);
             if (data.map.step == 9) {
-                $('.message').html("No one has won");
                 NewGame(data);
             }
         });
@@ -83,9 +82,10 @@ $(function () {
                 lines.visible = true; //grid
                 loading.remove(); //circle
                 Message("Socket Answer start_Map_", data.map);
-                if (data.map.winner) {
+                if (data.map.winner || data.map.step == 9) {
                     NewGame(data);
-                } else {
+                }
+                else {
                     $('#loading').css('visibility', 'hidden'); //text
                     $('.message').html("Game is started");
                     setTimeout(whichMove, 1000, data.map);
@@ -164,7 +164,9 @@ $(function () {
 
     function NewGame(data) {
         Message("User New Game", data.map);
-        if (data.map.winner == idUser) {
+        if (data.map.step == 9) {
+            $('.message').html("No one has won");
+        } else if (data.map.winner == idUser) {
             $('.message').html("You win");
         } else {
             $('.message').html("You lose");
